c4ec6e35a0741bd8c0ac2a730d50d1b2de9e2e3c
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2021-12-12 20:18:34 -0600

    added infos

---
+++ js/display.js
@@ -0,0 +1,11 @@
+function loadDoc(name) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", `./${name}.html`, true);
+  xhttp.send();
+}
