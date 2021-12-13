2f4dfd9c21b0239c26c93cd7d0684087d18f2468
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2021-12-13 06:58:16 -0600

    makign changes

--- js/display.js
+++ js/display.js
@@ -1,11 +1,17 @@
-function loadDoc(name) {
+
+
+  function loadDoc(name) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("main").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", `./${name}.html`, true);
+     if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("main-space").innerHTML =
+       this.responseText;
+     }
+
+}
+
+
+  xhttp.open("GET", `../${name}.html`, true);
+
   xhttp.send();
 }
