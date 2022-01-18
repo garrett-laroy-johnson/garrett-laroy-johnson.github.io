723771b4b835f547cdd0be07afeef592ed023f10
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2022-01-17 21:31:03 -0600

    addingportfolio

---
+++ portfolio/js/cosmic.js
@@ -0,0 +1,6 @@
+function beginFight () {alert("Changing gears, you accelerate passing under the 101 bearing east. You cross over the canal which is at once: ");
+alert("•	a transversal bore through the settler grid thrown down as an oppressive tabula rasa");
+alert("•	a cut in the valley demarcating the land into municipal parcels");
+alert("(goodbye Tempe, Hello Mesa)");
+alert("•	and a portacular relic of the Hohokam into the deep time of the engineered flow of life into the valley")
+}
